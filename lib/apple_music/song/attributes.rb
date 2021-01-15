192b3ec46239a458102b9d65dabd790530145b51
Virgile Delplace <41570195+virgiledelplace@users.noreply.github.com>
2021-01-15 17:23:31 +0100

    Update attributes.rb

--- lib/apple_music/song/attributes.rb
+++ lib/apple_music/song/attributes.rb
@@ -27,7 +27,11 @@ module AppleMusic
         @name = props['name'] # required
         @play_params = PlayParameters.new(props['playParams']) if props['playParams']
         @previews = Array(props['previews']).map { |attrs| Preview.new(attrs) } # required
-        @release_date = Date.parse(props['releaseDate']) # required
+        @release_date = begin # required
+                          Date.parse(props['releaseDate'])
+                        rescue ArgumentError
+                          Date.parse("#{props['releaseDate']}/01/01")
+                        end
         @track_number = props['trackNumber'] # required
         @url = props['url'] # required
         @work_name = props['workName']
